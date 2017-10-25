1da22abed0f11db51407eceaa760a7f73687de69
Philipp Danne <philipp@bandship.com>
2017-10-25 12:31:04 +0200

    Created home page and authentication

--- resources/views/pages/analysis.blade.php
+++ resources/views/pages/analysis.blade.php
@@ -5,8 +5,13 @@ Analyse
 @section('content')
 <div class="container">
     <section class="col-8 questions">
+        {!! Form::open(['action' => 'TempUserController@calculate', 'method' => 'POST']) !!}
+            echo Form::radio('stimmeZu', '1');
+            echo Form::radio('neutral', '2');
+            echo Form::radio('stimmeNichtZu', '3');
+        {!! Form::close() !!}
         <h1>Frage 1 von 10</h1>
-        <h2>Frage</h2>
+        <h2>{{ $fragenTitel }}</h2>
         <div class="container">
             <a class="col-md-4 col-sm-12 answer" href="#">stimme zu</a>
             <a class="col-md-4 col-sm-12 answer" href="#">neutral</a>
