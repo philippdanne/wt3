b6829f4c027e87f54b6fb0a2de01746aa450dad3
Philipp Danne <philipp@bandship.com>
2017-11-05 20:11:02 +0100

    Updated JQuery

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -22,6 +22,7 @@
       
     <!-- JavaScript & jQuery -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="../js/app.js"></script>
   </body>
 </html>
